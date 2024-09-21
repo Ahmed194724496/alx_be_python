25a3c6dee4bdafd72dbdcc5f741c3b21d9b82854
Ahmed194724496 <az7923140@gmail.com>
2024-09-21 20:53:16 +0300

    nitial commit for my Python project

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,18 +1,33 @@
+<<<<<<< HEAD
 num1 = int (input("Enter the first number: "))
 num2 = int (input("Enter the second number: "))
 operations = input ("Choose the operation (+, -, *, /): ")
+=======
+>>>>>>> 60757a3 (Initial commit for my Python project)
 
+# Prompt the user to input two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Prompt the user to choose an operation
+operations = input("Choose the operation (+, -, *, /): ")
+
+# Using match-case to perform the operation based on user input
 match operations: 
     case "+":
-        print(f"Result: {num1+num2}")  
-    case  "-":
-        print(f"Result: {num1-num2}")
+        print(f"The result is {num1 + num2}")
+    case "-":
+        print(f"The result is {num1 - num2}")
     case "*":
-        print(f"Result: {num1*num2}")
+        print(f"The result is {num1 * num2}")
     case "/":
         if num2 != 0:
-          print(f"Result: {num1 / num2}")
+            print(f"The result is {num1 / num2}")
         else:
             print("Error: Division by zero is not allowed")
     case _:
+<<<<<<< HEAD
         print("Invalid operation. Please choose one of +, -, *, /.")
+=======
+          print("Invalid operation. Please choose one of +, -, *, /.")
+>>>>>>> 60757a3 (Initial commit for my Python project)
---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,5 @@
+number= int(input("Enter a number to see its multiplication table: "))
+
+for X in range (1, 11):
+    result = X * number
+    print (F"{X}*{number}= {result}")
