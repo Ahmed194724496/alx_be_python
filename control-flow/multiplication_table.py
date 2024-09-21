484583cd3dd7c6051a91f669379e7dd7aeab475d
Ahmed Zidan <161758452+Ahmed194724496@users.noreply.github.com>
2024-09-21 21:19:53 +0300

    Update multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,9 @@
-number= int(input("Enter a number to see its multiplication table: "))
+# Request input from the user
+number = int(input("Enter a number to see its multiplication table: "))
 
-for X in range (1, 11):
-    result = X * number
-    print (F"{X}*{number}= {result}")
+# Loop through numbers 1 to 10
+for x in range(1, 11):
+    result = x * number
+    # Use f-string to print the multiplication table
+    print(f"{x} * {number} = {result}")
+
